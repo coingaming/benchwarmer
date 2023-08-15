6296656c17e23b6821b59ab939ddf656f00fbfa1
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2023-08-15 13:02:37 +0500

    fix: style for Elixir 1.15

--- mix.exs
+++ mix.exs
@@ -5,12 +5,12 @@ defmodule Benchwarmer.Mixfile do
     [
       app:          :benchwarmer,
       version:      "0.0.2",
-      elixir:       "~> 1.0.0-rc1",
-      deps:         deps,
+      elixir:       "~> 1.0",
+      deps:         deps(),
       name:         "Benchwarmer",
       source_url:   "https://github.com/mroth/benchwarmer",
-      description:  description,
-      package:      package
+      description:  description(),
+      package:      package()
     ]
   end
 
